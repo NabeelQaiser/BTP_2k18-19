fc4725c429e50baf0e4d3a5f08d530a0016b094b
NabeelQaiser <nabeel.bonvivant@gmail.com>
2019-02-19 13:07:51 +0530

    preprocessor

---
+++ PreProcessor.py
@@ -0,0 +1,59 @@
+class PreProcessor:
+
+    def __init__(self, fileSpec, fileData):
+        self.fileSpec = fileSpec
+        self.fileData = fileData
+
+    def getSpec(self):
+        with open(self.fileSpec, 'r') as f:
+            lines = f.readlines()
+            tableInfo = dict()      # format : {'tableName': [(attr, nullity), (attr, nullity), ...]}
+            constraints = []        # list of strings
+            isEnd = False
+            for line in lines:
+                if not isEnd:
+                    line = line.upper()
+                    line = line.strip()
+                    tokens = line.split('::')
+                    if len(tokens[0]) > 0:
+                        if tokens[0] == 'CONSTRAINTS':
+                            isEnd = True
+                            constraints = tokens[1].split(',')
+                            for i in range(len(constraints)):
+                                constraints[i] = constraints[i].strip()
+                                constraints[i] = constraints[i].strip('(')
+                                constraints[i] = constraints[i].strip(')')
+                        else:
+                            temp = []
+                            attr = tokens[1].split(',')
+                            for i in range(len(attr)):
+                                pair = (attr[i].split(':')[0].strip(), attr[i].split(':')[1].strip())
+                                temp.append(pair)
+                            tableInfo[tokens[0].strip()] = temp
+                else:
+                    break
+            return tableInfo, constraints
+
+
+
+    def start(self):
+        tableInfo, constraints = self.getSpec()
+        constraintString = ""
+        for i in constraints:
+            constraintString = constraintString + i + " AND "
+        constraintString = constraintString.strip(" AND ")
+        f = open(self.fileData, "r")
+        content = f.read().upper()
+        f.close()
+        temp = content.strip().split("BEGIN")
+        result = ""
+        result = result + temp[0] + "BEGIN\n\t" + "ASSUME " + constraintString + " ;\n" + temp[1]
+        for i in range(2, len(temp)):
+            result = result + "BEGIN" + temp[i]
+
+        temp = result.strip().split("END")
+        result = temp[0]
+        for i in range(1, len(temp)-1):
+            result = result + "END" + temp[i]
+        result = result + "ASSERT " + constraintString + " ;\n\t" + "END" + temp[len(temp)-1]
+        return tableInfo, constraints, result
