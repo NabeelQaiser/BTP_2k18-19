2b32a1cc6f3056b039eb58a32514806620dbde73
NabeelQaiser <nabeel.bonvivant@gmail.com>
2019-03-09 10:54:16 +0530

    tableDict update during JOIN in MyHelper

--- MyHelper.py
+++ MyHelper.py
@@ -202,9 +202,13 @@ class MyHelper(PlSqlVisitor):
                     res.add(tempCtx.children[i].getText())
                 #res.add(tempCtx.children[i].getText())
             elif self.getRuleName(tempCtx.children[i]) == "from_clause":
-                tableName = tempCtx.children[i].children[1].getText()
-                #print("**************************************************************", tableName, "\n\n\n\n\n\n\n\n\n")
-                res = res.union(self.tableDict[tableName])
+                # tableName = tempCtx.children[i].children[1].getText()
+                tableSet = set()
+                self.extractConditionsInFromClause(tempCtx.children[i].children[1], tableSet)
+                # print("**************************************************************", tableSet, "\n\n\n\n\n\n\n\n\n")
+                for tableName in tableSet:
+                    res = res.union(self.tableDict[tableName])
+                # res = res.union(self.tableDict[tableName])
             elif self.getRuleName(tempCtx.children[i]) == "where_clause":
                 #print("*****************************************where*********************", "\n\n\n\n\n\n\n\n\n")
                 self.temp = set()
@@ -212,6 +216,21 @@ class MyHelper(PlSqlVisitor):
                 res = res.union(self.temp)
                 self.temp = set()
         return res
+
+
+    # Recursive method to extract Conditions In From_Clause (SELECT, SELECT-IN-CURSOR)
+    def extractConditionsInFromClause(self, ctx, resultTableSet):  # ctx ~ from_clause.children[1]
+        if self.getRuleName(ctx) == "table_ref":
+            if ctx.getChildCount() == 2:
+                self.extractConditionsInFromClause(ctx.children[0], resultTableSet)
+                self.extractConditionsInFromClause(ctx.children[1], resultTableSet)
+            elif ctx.getChildCount() == 1:
+                resultTableSet.add(ctx.getText().strip())
+        elif self.getRuleName(ctx) == "join_clause":
+            for i in range(ctx.getChildCount()):
+                if self.getRuleName(ctx.children[i]) == "table_ref":
+                    resultTableSet.add(ctx.children[i].getText().strip())
+
 
     def updateTableDict(self, tableInfo):               #todo : solve insert issue for all the attributes
         for table in tableInfo:
