aeb85e70ed62ae9ce6ce3d8f91696ed97f316673
Moolchandra Mridul <aaalok5106@gmail.com>
2019-04-21 13:23:04 +0530

    minor changes

--- cnf/upper_input.sql
+++ cnf/upper_input.sql
@@ -1,34 +1,13 @@
-PROCEDURE TEST(M IN NUMBER) IS
-    MY_MAX NUMBER;
-    CURSOR G IS SELECT MAX(A) FROM T
-            WHERE A>=M+5 AND C IN (SELECT G FROM U WHERE D>=50);
-    BEGIN
-	ASSUME A>15 AND J>15 ;
+CREATE OR REPLACE PROCEDURE ATM_WITHDRAW_OPER( ACC_NO INT, AMT INT) IS
+BAL INT;
+MIN_BAL INT;
+BEGIN
+	ASSUME BALANCE>0 ;
 
-        OPEN G;
-        FETCH G INTO MY_MAX;
-        CLOSE G;
-        SELECT AVG(E), MAX(F) INTO X, Y FROM T
-                                  JOIN U ON B=D
-                                  JOIN V ON G=H
-                    WHERE E>=M+5 AND C<99;
-        IF MY_MAX > Y THEN
-            UPDATE V SET J=J*(X-9), F=Y-9 WHERE J>10 AND F<=(X+Y);
-        ELSIF MY_MAX < Y THEN
-            UPDATE T SET A=A*(X-9), B=Y-9 WHERE A>10 AND B<=(X+Y);
-        ELSE
-            INSERT INTO T(A, B, C) VALUES (MY_MAX-X, MY_MAX-Y, Y*(X-2));
+    SELECT BALANCE INTO BAL FROM ACCOUNT WHERE ACCNO = ACC_NO ;
+    MIN_BAL := BAL - AMT ;
+        IF AMT < 10000 AND MIN_BAL > 0 THEN
+            UPDATE ACCOUNT SET BALANCE = BALANCE-AMT WHERE ACCNO = ACC_NO ;
         END IF;
-    ASSERT A>0 AND J>10 ;
+ASSERT BALANCE>0 ;
 	END;
-
--- C2--G
--- T2--U
--- B2--D
--- A2--E
--- B3--F
--- T3--V
--- C3--H
--- A3--J
--- D2--K
--- D3--L
